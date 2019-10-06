9b1d48517cbff4e09db66de0d648c9505c76dd1d
Guadalupe Rom <55949213+Lusarom@users.noreply.github.com>
2019-10-06 01:52:15 -0500

    Add files via upload

---
+++ ejercicio21.py
@@ -0,0 +1,6 @@
+base = float(input('Ingresa base triangulo:'))
+altura = float(input('Ingresa altura triangulo:'))
+
+area = (base*altura)/2
+
+print('\n area triangulo: %.2f'% area,'elevado a la dos')
