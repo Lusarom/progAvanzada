0faa0a66dc4151e3e0d3195a051a5ab96e4643f1
Guadalupe Rom <55949213+Lusarom@users.noreply.github.com>
2019-10-06 12:32:02 -0500

    Add files via upload

---
+++ ejercicio29.py
@@ -0,0 +1,9 @@
+
+
+Tcelsius = float(input('Ingrese temperatura celsius:'))
+
+Tfahrenheit = Tcelsius * (9/5) + 32
+Tkelvin = Tcelsius + 273.15
+
+print('\n Temperatura fahrenheit: %.2f' %(Tfahrenheit))
+print('\n Temperatura kelvin: ',(Tkelvin))
