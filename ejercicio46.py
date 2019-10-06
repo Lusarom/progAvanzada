8adf91818ace73de323f1a9d94896755bcb76abb
Guadalupe Rom <55949213+Lusarom@users.noreply.github.com>
2019-10-05 22:49:47 -0500

    Add files via upload

---
+++ ejercicio46.py
@@ -0,0 +1,38 @@
+
+
+
+month=str(input('Inserta el mes:'))
+day=int(input('Inserta el dia:'))
+
+if month =='Enero' or month=='Febrero':
+    season='Invierno'
+elif month=='Marzo':
+    if day<20:
+        season='Invierno'
+else:
+    season='Primavera'
+
+if month == 'Abril' or month == 'Mayo':
+    season='Primavera'
+elif month=='Junio':
+    if day<21:
+        season='Primavera'
+else:
+    season='Verano'
+if month=='Julio' or month=='Agosto':
+            season="Verano"
+elif month=='Septiembre':
+            if day<22:
+                season='Verano'
+else:
+    season='Otoño'
+if month=='Octubre' or month=='Noviembre':
+                season='Otoño'
+elif month=='Diciembre':
+                if day<21:
+                    season='Otoño'
+                
+else:
+    season='Inverno'
+                
+print(month, day, 'la estacion es', season)
