1e5ebef9ce12734e452d599dd97efa3a0154b319
Guadalupe Rom <55949213+Lusarom@users.noreply.github.com>
2019-10-06 00:05:24 -0500

    Add files via upload

---
+++ ejercicio18.py
@@ -0,0 +1,7 @@
+import math
+rad=float(input('Ingresa radio del cilindro:'))
+altura=float(input('Ingresa altura del cilindro:'))
+
+vol=(math.pi*rad**2)*(altura)
+
+print('Volumen del cilindro: %.1f'%vol)
