98143dac33e0277b127680fbd469d49de015e823
Guadalupe Rom <55949213+Lusarom@users.noreply.github.com>
2019-10-05 20:35:58 -0500

    Add files via upload

---
+++ ejercicio34.py
@@ -0,0 +1,13 @@
+
+nent= float(int(input('Inserte un numero entero:')))
+
+n = (nent / 2)
+m = (nent % 2)
+
+if m <= 0.0:
+    
+    print('Tiene un numero par')
+    
+elif m >= 1:
+    
+    print('Tiene un numero impar')
