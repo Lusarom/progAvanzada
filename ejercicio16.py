992ff84704d777860c0cc9159c3f0de40d5bfa32
Guadalupe Rom <55949213+Lusarom@users.noreply.github.com>
2019-10-05 23:28:29 -0500

    Add files via upload

---
+++ ejercicio16.py
@@ -0,0 +1,11 @@
+import math
+
+radio=int(input('Ingresa el radio del circulo:'))
+
+area=math.pi*radio**2
+
+vol=(4/3)*math.pi*radio**3
+
+print('El area para el circulo es: ',str(area))
+
+print('El volumen para el circulo es: ',str(vol))
