d79f043331897205e32666d16d99944e3cfffad4
Guadalupe Rom <55949213+Lusarom@users.noreply.github.com>
2019-10-05 23:20:11 -0500

    Add files via upload

---
+++ ejercicio15.py
@@ -0,0 +1,11 @@
+
+
+feet= int(input('Ingrese distancia en feet: '))
+
+millas = feet * 0.000189
+yardas = feet * 0.333333
+inch = feet * 12
+
+print("\n a millas: %.6f." %(millas))
+print("\n a yardas: %.6f." %(yardas))
+print("\n a inch: %.6f." %(inch))
