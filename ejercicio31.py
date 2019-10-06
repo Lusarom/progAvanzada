e8bbbcb73c54b782d326bc32db4fc4021cc84460
Guadalupe Rom <55949213+Lusarom@users.noreply.github.com>
2019-10-06 13:23:40 -0500

    Add files via upload

---
+++ ejercicio31.py
@@ -0,0 +1,8 @@
+num = input('Ingrese numero entero a 4 digitos:')
+
+a = int(num[0])
+a += int(num[1])
+a += int(num[2])
+a += int(num[3])
+
+print(num[0], '+', num[1], '+', num[2], '+', num[3], '=',a)
