ede2e9d03619750f569563b7924903ccd2b6dc66
Guadalupe Rom <55949213+Lusarom@users.noreply.github.com>
2019-10-06 01:42:32 -0500

    Add files via upload

---
+++ ejercicio20.py
@@ -0,0 +1,13 @@
+Pressure=20000000
+
+Volume=12
+
+R=8.314
+
+Temperature=20
+
+n=int(input('Introduce el numero de moles:'))
+
+moles=(Pressure*Volume)/(R*Temperature)
+
+print('Numero de moles gas en tanque de buceo: ' ,moles)
