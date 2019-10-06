f479a0f91f6baab2dc3839b5fc4f2e9fdcf3fd6f
Guadalupe Rom <55949213+Lusarom@users.noreply.github.com>
2019-10-06 02:19:26 -0500

    Add files via upload

---
+++ ejercicio24.py
@@ -0,0 +1,15 @@
+
+Dias=float(input('Inserta Dias'))
+Horas=float(input('Inserta Horas'))
+Minutos=float(input('Inserta Minutos'))
+Segundos=float(input('Inserta Segundos'))
+
+Dias=Dias*86400
+Hora=Horas*3600
+Minuto=Minutos*60
+Segundos=Segundos
+
+Total=Dias+Horas+Minutos+Segundos
+
+print('\n total de segundos: %.2f.',Total)
+
