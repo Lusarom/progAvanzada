74cef9ec20a0b0189d5b83af75b5b82338d1d32b
Guadalupe Rom <55949213+Lusarom@users.noreply.github.com>
2019-11-04 22:47:41 -0600

    Add files via upload

---
+++ ejercicio57.py
@@ -0,0 +1,22 @@
+#Exercise 57: Is it a Leap Year?
+
+while True:
+    
+  year = int(input('Enter the year:\t'))
+  
+  yes = '%d is a leap year.' % (year)
+  no = '%d is not a leap year.' % (year)
+  
+  if year % 400 == 0:
+    print(yes)
+  
+  elif year % 100 == 0:
+    print(no)
+  
+  elif year % 4 == 0:
+    print(yes)
+  
+  else:
+    print(no)
+  
+  print()
